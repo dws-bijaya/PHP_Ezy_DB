5b1fd543e9fca52e7b6e18ecd7686048abb12dab
bijaya <bijaya.kumar@digitalwebsolutions.in>
2016-03-01 14:40:55 +0530

    Added sample database

---
+++ PHP_Ezy_Query.sql
@@ -0,0 +1,44 @@
+/*
+SQLyog Community v11.1 (32 bit)
+MySQL - 5.5.32 : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+USE `test`;
+
+/*Table structure for table `tgr_table1` */
+
+CREATE TABLE `tgr_table1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tgr_table1` */
+
+insert  into `tgr_table1`(`id`,`name`) values (1,'name 1'),(2,'name 2');
+
+/*Table structure for table `tgr_table2` */
+
+CREATE TABLE `tgr_table2` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `var` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tgr_table2` */
+
+insert  into `tgr_table2`(`id`,`var`) values (1,'var 1'),(2,'var 2');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
